cf8d1a7a11efea0c3d59285dd3ca354334f45e5d
Kyle Carter <kkcarter1129@gmail.com>
2019-10-30 16:55:15 -0500

    nav bar needs fixing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      menuStatus: "menu-close",
+      menuRef: React.createRef()
+    }
+  }
+
+  handleClick = e => {
+    const node = this.state.menuRef.current;
+    if (node.className === "menu-close") {
+      node.className = "menu-open";
+
+    } else {
+      node.className = "menu-close";
+    }
+  }
+
+  render() {
+    return (
+      <body>
+        <nav>
+          <h1 className="navTitle">Start Bootstrap</h1>
+          <img src="https://icon-library.net/images/menu-icon-white-png/menu-icon-white-png-27.jpg"
+            alt="navButton"
+            onClick={this.handleClick}
+          />
+          <ul id="navMenu">
+            <li>SERVICES</li>
+            <li>PORTFOLIO</li>
+            <li>ABOUT</li>
+            <li>TEAM</li>
+            <li>CONTACT</li>
+          </ul>
+        </nav>
+        <menu id="menu" ref={this.state.menuRef}>
+          <ul className="dropDownMenu">
+            <li>SERVICES</li>
+            <li>PORTFOLIO</li>
+            <li>ABOUT</li>
+            <li>TEAM</li>
+            <li>CONTACT</li>
+          </ul>
+        </menu>
+      </body>
+    );
+  }
 }
 
 export default App;
